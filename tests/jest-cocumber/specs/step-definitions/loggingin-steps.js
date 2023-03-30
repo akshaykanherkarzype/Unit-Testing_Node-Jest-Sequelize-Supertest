2ceffc414b6428f63e10262c17ff36a17e294892
akshaykanherkarzype <akshay.kanherkar@getzype.com>
2023-03-30 17:16:46 +0530

    Back

---
+++ tests/jest-cocumber/specs/step-definitions/loggingin-steps.js
@@ -0,0 +1,29 @@
+// logging-in.steps.js
+
+import { loadFeature, defineFeature } from 'jest-cucumber';
+import { PasswordValidator } from '../../src/password-validator';
+
+const feature = loadFeature('specs/features/LoggingIn.feature');
+
+defineFeature(feature, (test) => {
+  let passwordValidator = new PasswordValidator();
+  let accessGranted = false;
+
+  beforeEach(() => {
+    passwordValidator = new PasswordValidator();
+  });
+
+  test('Entering a correct password', ({ given, when, then }) => {
+    given('I have previously created a password', () => {
+      passwordValidator.setPassword('1234');
+    });
+
+    when('I enter my password correctly', () => {
+      accessGranted = passwordValidator.validatePassword('1234');
+    });
+
+    then('I should be granted access', () => {
+      expect(accessGranted).toBe(true);
+    });
+  });
+});
